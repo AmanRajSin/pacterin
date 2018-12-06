99b80a3259bbfdd9b1ff2f7533040db1c0ffed2b
Aman <30916747+Ghost-HawKk@users.noreply.github.com>
2018-12-06 16:44:15 +0530

    Delete font-awesome.js

--- controllers/font-awesome.js
+++
@@ -1,6 +0,0 @@
-module.exports  = function(app){
-    app.post('/configs/font-awesome', function(req, res){
-        res.writeHead(200, { 'Content-Type' : 'text/html'});
-        res.end('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">')
-    });
-};
