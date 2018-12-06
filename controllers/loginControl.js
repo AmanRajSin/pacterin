b2089542cd54f15ecd9f25f4cdc3459893cc4acb
Aman <30916747+Ghost-HawKk@users.noreply.github.com>
2018-12-06 22:02:34 +0530

    Add files via upload

--- controllers/loginControl.js
+++ controllers/loginControl.js
@@ -4,13 +4,20 @@ var dbController = require('./dbConnector.js');
 var con = dbController();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-con.connect(function(err) {/*if (err) throw err;*/});
+con.connect(function(err) {if (err) console.log(err);});
 
 module.exports = function(app){
     app.post('/login-submit', urlencodedParser, function(req, res) {        
-        con.query("SELECT * FROM user_auth WHERE username = ? AND password = ?", [req.body.username, req.body.password], function (err, result, fields) {
-            //if (err) throw err;
-            console.log(result);
+        con.query("SELECT * FROM user_auth WHERE username = ? AND password = ?", [req.body.username, req.body.password], function (q_err, result, fields) {
+            if(result) {
+                req.session.save(function(err) {
+                    if(err) console.log(err);
+                    else {
+                        req.session.username = req.body.username;
+                        res.redirect('/');
+                    }
+                });
+            }
         });
     });
 };
--- controllers/signupControl.js
+++ controllers/signupControl.js
@@ -8,9 +8,20 @@ con.connect(function(err) {/*if (err) throw err;*/});
 
 module.exports = function(app){
     app.post('/signup-submit', urlencodedParser, function(req, res) {        
-        con.query("INSERT INTO user_auth VALUES (?, ?)", [req.body.username, req.body.password], function (err, result, fields) {
-            //if (err) throw err;
-            console.log(result);
+        con.query("INSERT INTO user_auth VALUES (?, ?)", [req.body.username, req.body.password], function (q_err, result, fields) {
+            if (q_err) console.log(q_err);
+            con.query("INSERT INTO user_details VALUES (?, ?)", [req.body.username, req.body.email], function(q_err2, result2, fields2){
+                if(q_err2) console.log(q_err2);
+                else {
+                    req.session.save(function(err) {
+                        if(err) console.log(err);
+                        else {
+                            req.session.username = req.body.username;
+                            res.redirect('/');
+                        }
+                    });
+                }
+            });
         });
     });
 };
