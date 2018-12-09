45cbc2bbbccc9857d79cc6002aed49022b740591
SPIKE24 <45507891+SPIKE24@users.noreply.github.com>
2018-12-09 16:24:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-var express = require('express')
-var session = require('express-session')
-var loginControl = require('./controllers/loginControl')
-var signupControl = require('./controllers/signupControl')
-var usernameAvailability = require('./controllers/usernameAvailabilityCheck')
-var sessionControl = require('./controllers/sessionControl')
-var videoStream = require('./controllers/videoStream')
-var profileControl = require('./controllers/profileControl')
-var credEdit = require('./controllers/credEdit')
-var credChange = require('./controllers/credChange')
-var profEdit = require('./controllers/profEdit')
-var profChange = require('./controllers/profChange')
+const express = require('express')
+const loginControl = require('./controllers/loginControl')
+const signupControl = require('./controllers/signupControl')
+const usernameAvailability = require('./controllers/usernameAvailabilityCheck')
+const videoStream = require('./controllers/videoStream')
+const profileControl = require('./controllers/profileControl')
+const credEdit = require('./controllers/credEdit')
+const credChange = require('./controllers/credChange')
+const profEdit = require('./controllers/profEdit')
+const profChange = require('./controllers/profChange')
+const logout = require('./controllers/logout')
+const serverControl = require('./controllers/serverControl')
 
 var app = express();
-app.set('view engine', 'ejs');
-app.use(session(sessionControl));
 
 // Fire Controllers
 loginControl(app);
@@ -25,15 +23,5 @@ credEdit(app);
 credChange(app);
 profEdit(app);
 profChange(app);
-
-// Static files
-app.use(express.static('./public'));
-
-// Request Handle
-app.get('/', function(req, res) { res.render('./index');});
-app.get('/login', function(req, res) { res.render('./login');});
-app.get('/signup', function(req, res) { res.render('./signup');});
-
-// Listen to Port
-app.listen(process.env.PORT||80);
-console.log('Port 80 Up!');
+logout(app);
+serverControl(app);
