6f6286a6187eb1684ddc82181236e0b7d9008d69
Aman <30916747+Ghost-HawKk@users.noreply.github.com>
2018-12-10 15:22:06 +0530

    Add files via upload

--- controllers/socketControl.js
+++ controllers/socketControl.js
@@ -1,10 +1,34 @@
 const express = require('express')
 
+var userSocketId = {};
+
 module.exports = function(server) {
     var io = require('socket.io')(server)
+    var chat = io.of('/messenger');
 
-    io.on('connection', function(client_socket){
-        console.log("New User Connected!!");
-        
+    chat.on('connection', function(socket){
+        console.log("New User Connected : " + socket.request._query['foo']);
+
+        socket.on('change_username', function(data) {
+            socket.username = data.from;
+            userSocketId[socket.username] = socket.id;
+            console.log(userSocketId);
+        });
+
+        socket.on('new_message', function(data){
+            chat.to(userSocketId[data.to]).emit('new_message', {from: socket.username, message: data.message});
+        });
+
+        socket.on('disconnect', function(){
+            delete userSocketId[socket.username];
+            console.log(userSocketId);
+        });
+
+        socket.on('user_typing', function(data){
+            chat.to(userSocketId[data.to]).emit('get_user_typing', {from: socket.username})
+        });
+        socket.on('user_not_typing', function(data){
+            chat.to(userSocketId[data.to]).emit('get_user_not_typing', {from: socket.username})
+        });
     });
-};
+}
