0bcbd213edbb5f59a98fd27337128c853f59d613
SPIKE24 <45507891+SPIKE24@users.noreply.github.com>
2018-12-10 21:06:35 +0530

    Update loginControl.js

--- controllers/loginControl.js
+++ controllers/loginControl.js
@@ -17,6 +17,8 @@ module.exports = function(app){
                         res.redirect('/');
                     }
                 });
+            }else{
+                res.redirect('/login');
             }
         });
     });
